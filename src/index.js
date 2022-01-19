290755edd51cfc8b1d0d5143b9d88046ffbba2c8
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-01-19 16:07:19 +0200

    remove dotenv.config()

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
-const dotEnv = require('dotenv');
 
 async function startApolloServer(typeDefs, resolvers) {
-  dotEnv.config();
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
